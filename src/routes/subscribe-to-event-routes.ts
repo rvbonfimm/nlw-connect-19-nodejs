1a31eb7ed59ae3baf74d12fa41a386ed1954cf72
rvbonfimm <rogeriovilelabonfim@gmail.com>
2025-02-22 14:54:15 -0300

    refactor: update filename

--- src/routes/subscribe-to-event-routes.ts
+++ src/routes/subscribe-to-event-routes.ts
@@ -1,7 +1,7 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 
-export const subscribeRoute: FastifyPluginAsyncZod = async app => {
+export const subscribeToEventRoute: FastifyPluginAsyncZod = async app => {
   app.post(
     '/subscriptions',
     {
